719c8419729270ae54153fc1bd139f78f8f6ec48
Mateusz Biel <matbiel@gmail.com>
2024-03-01 00:09:45 +0100

    Add ArrayList.java

--- java/src/main/java/org/algorithms/Main.java
+++ java/src/main/java/org/algorithms/Main.java
@@ -1,7 +1,17 @@
 package org.algorithms;
 
+import org.algorithms.list.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    ArrayList<Number> list = new ArrayList<>(2);
+    list.addLast(1);
+    list.addLast(2);
+    list.addLast(3.2f);
+    list.forEach(e -> System.out.println(e));
+    list.removeLast();
+    list.removeLast();
+    System.out.println("After remove");
+    list.forEach(e -> System.out.println(e));
   }
 }
---
+++ java/src/main/java/org/algorithms/list/ArrayList.java
@@ -0,0 +1,57 @@
+package org.algorithms.list;
+
+import java.util.function.Consumer;
+
+public class ArrayList<T> {
+  private T[] values;
+  private int size;
+  private int allocated;
+
+  @SuppressWarnings("unchecked")
+  public ArrayList(int initialAllocated) {
+    this.size = 0;
+    this.allocated = initialAllocated;
+    this.values = (T[]) new Object[this.allocated];
+  }
+
+  public ArrayList() {
+    this(4);
+  }
+
+  @SuppressWarnings("unchecked")
+  public void addLast(T value) {
+    if (this.size + 1 > this.allocated) {
+      int newAllocated = this.allocated << 1;
+      T[] newArray = (T[]) new Object[newAllocated];
+      for (int idx = 0; idx < this.size; idx++) {
+        newArray[idx] = this.values[idx];
+      }
+      this.allocated = newAllocated;
+      this.values = newArray;
+    }
+    this.values[this.size] = value;
+    this.size += 1;
+  }
+
+  @SuppressWarnings("unchecked")
+  public void removeLast() {
+    if (this.size > 0) {
+      int newSize = this.size - 1;
+      int newAllocated = this.allocated >> 1;
+      if (newSize < newAllocated) {
+        T[] newArray = (T[]) new Object[newAllocated];
+        for (int idx = 0; idx < newSize; idx++) {
+          newArray[idx] = this.values[idx];
+        }
+      }
+      this.size = newSize;
+      this.allocated = newAllocated;
+    }
+  }
+
+  public void forEach(Consumer<T> consumer) {
+    for (int idx = 0; idx < this.size; idx++) {
+      consumer.accept(this.values[idx]);
+    }
+  }
+}
